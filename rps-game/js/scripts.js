e1b8db70e6953bb4292b4ce654c639b94fb69ff3
Sliwinski, Maciej <Maciej.Sliwinski@sabre.com>
2019-12-24 00:11:02 +0100

    random id to choose

--- rps-game/js/scripts.js
+++ rps-game/js/scripts.js
@@ -2,33 +2,31 @@ function rpsGame(yourChoice) {
     console.log(yourChoice);
 
     let humanChoice = yourChoice.id;
-    
+
     console.log(humanChoice);
 
-    let botChoice = intToIdOfChoice(randomChoice()) ;  //random function convert to {0,1,2} where 0 means rock, 1->paper, 2->scissors
+    let botChoice = intToIdOfChoice(randomChoice()); //random function convert to {0,1,2} where 0 means rock, 1->paper, 2->scissors
 
-    let winner = decideWhoWon(humanChoice, botChoice);    
+    console.log(botChoice);
+    // let winner = decideWhoWon(humanChoice, botChoice);    
 
-    let message = showMessage(winner); //message who won {You won, You lose, You draw} {color: green, red, gray}
+    // let message = showMessage(winner); //message who won {You won, You lose, You draw} {color: green, red, gray}
 }
 
 
- function decideWhoWon(humanChoice, botChoice) {
+//  function decideWhoWon(humanChoice, botChoice) {
 
-    
-}
 
-function showMessage(whoIsWon) {
-    
-}
+// }
+
+// function showMessage(whoIsWon) {
+
+// }
 
 function randomChoice() {
-    return Math.floor(Math.random() * 3); 
+    return Math.floor(Math.random() * 3);
 }
 
 function intToIdOfChoice(number) {
-        let options = {
-            
-        }
+    return ['rock', 'paper', 'scissors'][number];
 }
-
