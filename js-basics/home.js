8ea12a83e9ecb527df78941e13e0f9bc70b43f84
Sliwinski, Maciej <Maciej.Sliwinski@sabre.com>
2019-12-23 11:55:16 +0100

    add game, learning js

---
+++ js-basics/home.js
@@ -0,0 +1,48 @@
+console.log('hello world');
+// alert("I am live");
+
+var a = 'this is string';
+
+console.log(a);
+
+document.getElementById('pid').innerHTML='hey there on page';
+
+// var message = '';
+var name = prompt('what is your name?');
+
+document.getElementById('pid').innerHTML = name;
+
+
+// showInParagraf(message);
+// function showInParagraf() {
+//     document.getElementById('pid').innerHTML = message;
+// }
+
+
+//numbers in js
+
+var num1 = 5.7;
+
+console.log(5*10);
+
+//functions
+
+//call
+fun(); 
+
+//def
+function fun() {
+    alert('this is function');
+}
+
+
+
+function greeting() {
+    var name = prompt("what is your name?");
+    var result = 'Hello' + ' ' + name;
+    console.log(result)
+    
+}
+
+greeting();
+
