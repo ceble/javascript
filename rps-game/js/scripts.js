d328947f771bbff85d3fddf7e4416495016b72c5
Sliwinski, Maciej <Maciej.Sliwinski@sabre.com>
2019-12-31 19:06:21 +0100

    almost finished

--- rps-game/js/scripts.js
+++ rps-game/js/scripts.js
@@ -3,25 +3,58 @@ function rpsGame(yourChoice) {
 
     let humanChoice = yourChoice.id;
 
-    console.log(humanChoice);
-
     let botChoice = intToIdOfChoice(randomChoice()); //random function convert to {0,1,2} where 0 means rock, 1->paper, 2->scissors
 
-    console.log(botChoice);
-    // let winner = decideWhoWon(humanChoice, botChoice);    
+    let winner = decideWhoWon(humanChoice, botChoice);
+    console.log(humanChoice + " " + botChoice);
+    console.log(winner);
 
-    // let message = showMessage(winner); //message who won {You won, You lose, You draw} {color: green, red, gray}
+    let message = showMessage(winner); //message who won {You won, You lose, You draw} {color: green, red, gray}
+    console.log(message);
+
 }
 
 
-//  function decideWhoWon(humanChoice, botChoice) {
+function decideWhoWon(humanChoice, botChoice) {
+    scoreTable = {
+        'rock': {
+            'rock': 0.5,
+            'paper': 0,
+            'scissors': 1
+        },
+        'paper': {
+            'rock': 1,
+            'paper': 0.5,
+            'scissors': 0
+        },
+        'scissors': {
+            'rock': 0,
+            'paper': 1,
+            'scissors': 0.5
+        }
+    };
 
+    return scoreTable[humanChoice][botChoice];
+}
 
-// }
+function showMessage(humanScore) {
+    if (humanScore === 1)
+        return {
+            message: 'You won',
+            color: 'green'
+        };
+    else if (humanScore === 0.5)
+        return {
+            message: 'You tied',
+            color: 'yellow'
+        }
+    else
+        return {
+            message: 'You lose',
+            color: 'red'
+        }
 
-// function showMessage(whoIsWon) {
-
-// }
+}
 
 function randomChoice() {
     return Math.floor(Math.random() * 3);
